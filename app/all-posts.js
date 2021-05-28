50a43b2ea5eb1283037a5995e4cdb2e01d837c14
nsachin08 <53273271+nsachin08@users.noreply.github.com>
2021-05-28 10:46:31 +0530

    Update all-posts.js
    Adding filter XSS

--- app/all-posts.js
+++ app/all-posts.js
@@ -3,10 +3,10 @@ function loadPosts() {
       for (let p of posts) {
         $('#content').append(
           $(`
-            <div class="card rounded m-2  " id="${p._id}">
+            <div class="card rounded m-2  " id="${filterXSS(p._id)}">
             <div class="card-body">
             <div class="tp">
-            <div class="card-title">Name: ${p.name}</div>
+            <div class="card-title">Name: ${filterXSS(p.name)}</div>
             <div class="dt">
             <div >${p.createdAt.slice(0,10)}</div>
             <div >${p.createdAt.slice(11,19)}</div>
@@ -14,12 +14,12 @@ function loadPosts() {
             </div>
             <div>
             <div class="tq">
-            <div class="card-text">${p.caption}</div> 
+            <div class="card-text">${filterXSS(p.caption)}</div> 
             <a class="delete" href="https://qmeme-project.herokuapp.com/memes/delete/${p._id}"><i class="fa fa-trash"></i></a>
             </div>
             </div>
             </div>
-            <img class="card-img-top" src="${p.url}">
+            <img class="card-img-top" src="${filterXSS(p.url)}">
             </div>
           `)
         )
